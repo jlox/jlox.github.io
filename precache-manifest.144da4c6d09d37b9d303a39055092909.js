cfb229b6e3b77eb8d4d31cbc2f30c9df37c6e0e6
jeanne locker <jealocker@gmail.com>
2020-09-17 21:38:36 -0400

    Updates

---
+++ precache-manifest.144da4c6d09d37b9d303a39055092909.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ea3bf4717740d46e6d7437581cfc99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2900b7504906c9c75e7",
+    "url": "/static/css/main.95aea8dc.chunk.css"
+  },
+  {
+    "revision": "b10a8a050c95880a5891",
+    "url": "/static/js/2.1ac5481a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ac5481a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2900b7504906c9c75e7",
+    "url": "/static/js/main.f67bd655.chunk.js"
+  },
+  {
+    "revision": "cc3b2e95ee1b5b02baa1",
+    "url": "/static/js/runtime-main.77b3c05b.js"
+  },
+  {
+    "revision": "dc0206c676468575d67f52165bdee871",
+    "url": "/static/media/IMG_7993.dc0206c6.jpg"
+  },
+  {
+    "revision": "34b6e2fda5afbbd0b10a84b08d788aa3",
+    "url": "/static/media/JeanneLockerResume.34b6e2fd.pdf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
